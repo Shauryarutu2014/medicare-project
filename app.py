53bb5f0a7f6adc766c248a285c31b6d663a7a56e
Shaurya Nitin Gadekar <shauryang2014@gmail.com>
2026-05-16 10:52:57 +0530

    Fixed Gmail SMTP

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ app.secret_key = "medicare_secret"
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get("EMAIL_USER")
+app.config['MAIL_PASSWORD'] = os.environ.get("EMAIL_PASS")
 
 app.config['MAIL_USERNAME'] = "medicareweb2026@gmail.com"
 app.config['MAIL_PASSWORD'] = "xpis ackf rmzy svgv"

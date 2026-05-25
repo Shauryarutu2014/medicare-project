11b4b7f21d35233d0feb223ce3a3ac225a81b655
Shaurya Nitin Gadekar <shauryang2014@gmail.com>
2026-05-25 11:55:33 +0530

    Upload Medicine Suggestion

--- Hash.py
+++ Hash.py
@@ -1,3 +1,7 @@
 from werkzeug.security import generate_password_hash
 
+print(generate_password_hash("27052014"))   # Shaurya Gadekar
+print(generate_password_hash("09112013"))   # Harsh Thakre
+
+print (generate_password_hash("nsrshauryagadekar2014"))
 print(generate_password_hash("smrharshthakre2013"))
